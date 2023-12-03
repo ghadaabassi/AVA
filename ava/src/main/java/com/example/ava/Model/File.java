d8f0d8e54b398eb9d0f40dc4e16f3a30a3d3ed16
ghada <gh.abassi01@gmail.com>
2023-12-03 19:24:23 +0100

    Adjust_File_Size

--- ava/src/main/java/com/example/ava/Model/File.java
+++ ava/src/main/java/com/example/ava/Model/File.java
@@ -1,9 +1,11 @@
 package com.example.ava.Model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 
 @Entity
 public class File {
@@ -13,6 +15,8 @@ public class File {
 
     private String fileName;
 
+    @Lob
+    @Column(length = 1000000000)
     private byte[] data;
 
     public File() {
